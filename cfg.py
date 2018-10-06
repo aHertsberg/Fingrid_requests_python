2dced717193b13355939d9bdc63d8e691301fee0
Hertsberg Axel <hertsba1@kosh.org.aalto.fi>
2018-10-06 18:03:54 +0300

    made two axes in a figure, one with total production and one with all components

--- cfg.py
+++ cfg.py
@@ -1,3 +1,7 @@
 # insert API key here
 global API_key
 API_key = ''
+
+var_IDs = {'Total power production in Finland':'192', 'Condensing':'189', \
+        'Combined heat and power':'201', 'Nuclear': '188', 'Wind':'181', \
+        'Hydro':'191', 'Other':'205', 'Industrial CHP':'202'}
--- fetch.py
+++ fetch.py
@@ -4,29 +4,46 @@ import requests
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import cfg
 
 
-print(cfg.API_key)
 API_key = cfg.API_key
 
 headers = {'x-api-key':API_key, 'Accept':'application/json'}
-var_IDs = [198,]
-var_IDs = map(str, var_IDs)
-payload = {'start_time':'2018-07-18T08:00:00+0300', 'end_time':'2018-07-18T12:00:00+0300'}
+variable_dict = cfg.var_IDs
+variable_names = variable_dict.keys()
 url = 'https://api.fingrid.fi/v1/variable/{}/events/json'
+
+now = datetime.now()
+start = now - timedelta(days=1)
+payload = {'start_time':'{}:00+0300'.format(datetime.strftime(start, '%Y-%m-%dT%H:%M')), 'end_time':'{}:00+0300'.format(datetime.strftime(now, '%Y-%m-%dT%H:%M'))}
 
 frequencies = []
 timestamps = []
-for var in var_IDs:
-    r = requests.get(url.format(var), headers=headers, params=payload)
-    for e in r.json():
-        print(e)
 
-        frequencies.append(float(e['value']))
+
+plt.figure('production', dpi=100, figsize=(16,8))
+for variable in variable_names:
+    if 'Total power production' in variable:
+        ax = plt.subplot(212)
+    else:
+        ax = plt.subplot(211)
+    values = []
+    timestamps = []
+    r = requests.get(url.format(variable_dict[variable]), headers=headers, params=payload)
+    for e in r.json():
+        values.append(float(e['value']))
         timestamps.append(datetime.strptime(e['start_time'], '%Y-%m-%dT%H:%M:%S+0000'))
 
-plt.figure()
-plt.plot(timestamps, frequencies)
-plt.grid(b=True)
-plt.savefig('figurename.png')
+    ax.plot(timestamps, values, label=variable)
+
+ax = plt.subplot(211)
+ax.legend(loc='upper center', bbox_to_anchor=(.5, 1.20), ncol=4, fancybox=True)
+ax.grid(b=True)
+ax.fmt_xdata = mdates.DateFormatter('%H:%M')
+
+ax = plt.subplot(212)
+ax.grid(b=True)
+plt.title('Power production in Finland')
+plt.savefig('Production.png')
